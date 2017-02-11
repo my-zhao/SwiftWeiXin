554e57d562c433f5dc26d5e9c9cb90faeaedc33c
my-zhao <my_zhao@126.com>
2017-02-11 13:51:46 +0800

    build a project

---
+++ SwiftWeiXin/SwiftWeiXin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWeiXin
+//
+//  Created by ZhaoMingyang on 2017/2/11.
+//  Copyright © 2017年 ZhaoMingyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
